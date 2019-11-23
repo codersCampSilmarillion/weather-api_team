312aaddd4046dc488284368f7cc25364e5ec6af6
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-11-23 19:43:24 +0100

    Added index.js
    Now when the city name is inserted, a bgColor changes according to weather

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import "./styles.scss";
+import "./styles.scss"; \\nie wiem po co to tu i czy to ma być tu
+
+
+function searchWeather(searchedCity) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&APPID=0599898336f8892a2625cd0151ec957c&units=metric`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function init(resultFromServer) {
+    (resultFromServer.weather[0].id <= 232) ? document.body.style.backgroundColor = 'black': //thunderstorm
+        (resultFromServer.weather[0].id <= 321) ? document.body.style.backgroundColor = 'darkgrey' : //drizzle
+        (resultFromServer.weather[0].id <= 531) ? document.body.style.backgroundColor = 'blue' : //rain
+        (resultFromServer.weather[0].id <= 622) ? document.body.style.backgroundColor = 'ghostwhite' : //snow
+        (resultFromServer.weather[0].id <= 781) ? document.body.style.backgroundColor = 'gold' : //atmosphere
+        (resultFromServer.weather[0].id == 800) ? document.body.style.backgroundColor = 'aqua' : //clear sky
+        document.body.style.backgroundColor = 'fuchsia'; //clouds;
+}
+
+document.getElementById('searchbutton').addEventListener('click', () => {
+    let searchedCity = document.getElementById("searchInput").value;
+    searchWeather(searchedCity)
+})
