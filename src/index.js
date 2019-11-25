17707d4898a35e0b54fc53653442780283f91c99
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-11-25 18:14:57 +0100

    Dodane funkcje do index.js
    index.js zawiera: funkcję zmieniającą tło, funkcję wyświetlającą prognozę na 5 kolejnych dni  (dzień tygodnia, temperaturę, datę i odpowiednią ikonkę) oraz funkcję pomocniczą zamieniającą stopnie Fahrenheita na stopnie Celsjusza w przybliżeniu. Wszystkie funkcje działają zarówno dla współrzędnych geograficznych pobranych na podstawie lokalizacji użytkownika oraz na podstawie wpisanego przez użytkownika miasta, dla którego chce on sprawdzić prognozę.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-//funkcję backgroundChange chciałem dać do innego pliku i ją wyeksportować, jednak nie chce mi to działać, nie wiem dlaczego
+//pobranie danych geograficznych od użytkownika
+var geo = navigator.geolocation;
 
-function backgroundChange(resultFromServer) {
-    (resultFromServer.weather[0].id <= 232) ? document.body.style.backgroundColor = 'black': //thunderstorm
-        (resultFromServer.weather[0].id <= 321) ? document.body.style.backgroundColor = 'darkgrey' : //drizzle
-        (resultFromServer.weather[0].id <= 531) ? document.body.style.backgroundColor = 'blue' : //rain
-        (resultFromServer.weather[0].id <= 622) ? document.body.style.backgroundColor = 'ghostwhite' : //snow
-        (resultFromServer.weather[0].id <= 781) ? document.body.style.backgroundColor = 'gold' : //atmosphere
-        (resultFromServer.weather[0].id == 800) ? document.body.style.backgroundColor = 'aqua' : //clear sky
-        document.body.style.backgroundColor = 'fuchsia'; //clouds
-    console.log(resultFromServer.weather[0].id)
+if (geo) {
+    geo.getCurrentPosition(function(location) {
+        var lat = Math.round(location.coords.latitude);
+        var lon = Math.round(location.coords.longitude);
+        searchWeatherByLocation(lat, lon);
+        dailyPrognoseByLocation(lat, lon);
+    });
+} else {
+    console.log('niedostępny');
 }
 
+document.getElementById("searchButton").addEventListener('click', () => {
+    let searchedCity = document.getElementById("searchInput").value;
+    searchWeatherByCity(searchedCity)
+    dailyPrognoseByCity(searchedCity);
+    event.preventDefault()
+})
 
 //funkcja, która na podstawie danych geograficznych użytkownika pokazuje odpowiednią pogode (należy dodać) i zmienia kolor tła(to już jest)
 function searchWeatherByLocation(lat, lon) {
@@ -21,20 +28,15 @@ function searchWeatherByLocation(lat, lon) {
     })
 };
 
-//pobranie danych geograficznych od użytkownika
-var geo = navigator.geolocation;
-
-if (geo) {
-    geo.getCurrentPosition(function(location) {
-        var lat = Math.round(location.coords.latitude);
-        var lon = Math.round(location.coords.longitude);
-        searchWeatherByLocation(lat, lon)
-        dailyPrognose(lat, lon)
-    });
-} else {
-    console.log('niedostępny');
+//funkcja pobierająca dane o prognozie na 5dni na podstawie danych geograficznych
+function dailyPrognoseByLocation(lat, lon) {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=0599898336f8892a2625cd0151ec957c`).then(result => {
+        return result.json();
+    }).then(result => {
+        console.log(result);
+        thisWeek(result)
+    })
 }
-
 
 
 //funkcja, która na podstawie wprowadzonego miasta przez użytkownika pokazuje odpowiednią pogode (należy dodać) i zmienia kolor tła(to już jest)
@@ -46,24 +48,27 @@ function searchWeatherByCity(searchedCity) {
     })
 }
 
-document.getElementById('searchbutton').addEventListener('click', () => {
-    let searchedCity = document.getElementById("searchInput").value;
-    searchWeatherByCity(searchedCity)
-})
 
-//funkcja pobierająca dane o prognozie na 5dni
-function dailyPrognose(lat, lon) {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=0599898336f8892a2625cd0151ec957c`).then(result => {
+//funkcja pobierająca dane o prognozie na 5dni na podstawie wpisanego przez użytkownika miasta
+function dailyPrognoseByCity(searchedCity) {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchedCity}&APPID=0599898336f8892a2625cd0151ec957c`).then(result => {
         return result.json();
     }).then(result => {
         console.log(result);
         thisWeek(result)
     })
 }
+
+//funckja zamieniająca fahrenheity na celsiuse w przyblizeniu
+function celsius(fahrenheit) {
+    return `${Math.round(fahrenheit-273.15)}°C`
+}
+
 //funkcja pokazująca prognozę na 5 dni, nie działa jeszcze zmiana daty:(
 function thisWeek(resultFromServer) {
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday'];
     now = new Date();
+    let daysSume = now.getDate()
     let day1 = document.getElementById("day1");
     let day1Temp = document.getElementById("day1temp");
     let day1Date = document.getElementById("day1date");
@@ -90,23 +95,43 @@ function thisWeek(resultFromServer) {
     let day5Icon = document.getElementById("day5icon");
 
     day1.innerHTML = `${days[now.getDay()+1]}`;
-    day1Temp.innerHTML = `${Math.round(resultFromServer.list[7].main.temp-273.15)}°C`;
+    day1Temp.innerHTML = celsius(resultFromServer.list[7].main.temp);
+    now.setUTCDate(daysSume + 1)
+    day1Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
     day1Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[7].weather[0].icon + '.png';
-    console.log(Math.round(resultFromServer.list[15].main.temp - 273.15))
-    day2.innerHTML = `${days[now.getDay()+2]}`;
-    day2Temp.innerHTML = `${Math.round(resultFromServer.list[15].main.temp-273.15)}°C`;
 
+    day2.innerHTML = `${days[now.getDay()+1]}`;
+    day2Temp.innerHTML = celsius(resultFromServer.list[15].main.temp)
+    now.setUTCDate(daysSume + 2)
+    day2Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
     day2Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[15].weather[0].icon + '.png';
 
-    day3.innerHTML = `${days[now.getDay()+3]}`;
-    day3Temp.innerHTML = `${Math.round(resultFromServer.list[23].main.temp-273.15)}°C`;
+    day3.innerHTML = `${days[now.getDay()+1]}`;
+    day3Temp.innerHTML = celsius(resultFromServer.list[23].main.temp);
+    now.setUTCDate(daysSume + 3)
+    day3Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
     day3Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[23].weather[0].icon + '.png';
 
-    day4.innerHTML = `${days[now.getDay()+4]}`;
-    day4Temp.innerHTML = `${Math.round(resultFromServer.list[31].main.temp-273.15)}°C`;
+    day4.innerHTML = `${days[now.getDay()+1]}`;
+    day4Temp.innerHTML = celsius(resultFromServer.list[31].main.temp);
+    now.setUTCDate(daysSume + 4)
+    day4Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
     day4Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[31].weather[0].icon + '.png';
 
-    day5.innerHTML = `${days[now.getDay()+5]}`;
-    day5Temp.innerHTML = `${Math.round(resultFromServer.list[39].main.temp-273.15)}°C`;
+    day5.innerHTML = `${days[now.getDay()+1]}`;
+    day5Temp.innerHTML = celsius(resultFromServer.list[39].main.temp);
+    now.setUTCDate(daysSume + 5)
+    day5Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
     day5Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[39].weather[0].icon + '.png';
 };
+
+//funkcja zmieniająca tło
+function backgroundChange(resultFromServer) {
+    (resultFromServer.weather[0].id <= 232) ? document.body.style.backgroundColor = 'black': //thunderstorm
+        (resultFromServer.weather[0].id <= 321) ? document.body.style.backgroundColor = 'darkgrey' : //drizzle
+        (resultFromServer.weather[0].id <= 531) ? document.body.style.backgroundColor = 'blue' : //rain
+        (resultFromServer.weather[0].id <= 622) ? document.body.style.backgroundColor = 'ghostwhite' : //snow
+        (resultFromServer.weather[0].id <= 781) ? document.body.style.backgroundColor = 'gold' : //atmosphere
+        (resultFromServer.weather[0].id == 800) ? document.body.style.backgroundColor = 'aqua' : //clear sky
+        document.body.style.backgroundColor = 'fuchsia'; //clouds
+}
