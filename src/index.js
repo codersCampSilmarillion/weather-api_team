c4328acef2376543dfa4d6650c19db0b8104e99e
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-11-25 11:56:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ if (geo) {
         var lat = Math.round(location.coords.latitude);
         var lon = Math.round(location.coords.longitude);
         searchWeatherByLocation(lat, lon)
+        dailyPrognose(lat, lon)
     });
 } else {
     console.log('niedostępny');
@@ -50,5 +51,62 @@ document.getElementById('searchbutton').addEventListener('click', () => {
     searchWeatherByCity(searchedCity)
 })
 
+//funkcja pobierająca dane o prognozie na 5dni
+function dailyPrognose(lat, lon) {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=0599898336f8892a2625cd0151ec957c`).then(result => {
+        return result.json();
+    }).then(result => {
+        console.log(result);
+        thisWeek(result)
+    })
+}
+//funkcja pokazująca prognozę na 5 dni, nie działa jeszcze zmiana daty:(
+function thisWeek(resultFromServer) {
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday'];
+    now = new Date();
+    let day1 = document.getElementById("day1");
+    let day1Temp = document.getElementById("day1temp");
+    let day1Date = document.getElementById("day1date");
+    let day1Icon = document.getElementById("day1icon");
 
+    let day2 = document.getElementById("day2");
+    let day2Temp = document.getElementById("day2temp");
+    let day2Date = document.getElementById("day2date");
+    let day2Icon = document.getElementById("day2icon");
 
+    let day3 = document.getElementById("day3");
+    let day3Temp = document.getElementById("day3temp");
+    let day3Date = document.getElementById("day3date");
+    let day3Icon = document.getElementById("day3icon");
+
+    let day4 = document.getElementById("day4");
+    let day4Temp = document.getElementById("day4temp");
+    let day4Date = document.getElementById("day4date");
+    let day4Icon = document.getElementById("day4icon");
+
+    let day5 = document.getElementById("day5");
+    let day5Temp = document.getElementById("day5temp");
+    let day5Date = document.getElementById("day5date");
+    let day5Icon = document.getElementById("day5icon");
+
+    day1.innerHTML = `${days[now.getDay()+1]}`;
+    day1Temp.innerHTML = `${Math.round(resultFromServer.list[7].main.temp-273.15)}°C`;
+    day1Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[7].weather[0].icon + '.png';
+    console.log(Math.round(resultFromServer.list[15].main.temp - 273.15))
+    day2.innerHTML = `${days[now.getDay()+2]}`;
+    day2Temp.innerHTML = `${Math.round(resultFromServer.list[15].main.temp-273.15)}°C`;
+
+    day2Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[15].weather[0].icon + '.png';
+
+    day3.innerHTML = `${days[now.getDay()+3]}`;
+    day3Temp.innerHTML = `${Math.round(resultFromServer.list[23].main.temp-273.15)}°C`;
+    day3Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[23].weather[0].icon + '.png';
+
+    day4.innerHTML = `${days[now.getDay()+4]}`;
+    day4Temp.innerHTML = `${Math.round(resultFromServer.list[31].main.temp-273.15)}°C`;
+    day4Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[31].weather[0].icon + '.png';
+
+    day5.innerHTML = `${days[now.getDay()+5]}`;
+    day5Temp.innerHTML = `${Math.round(resultFromServer.list[39].main.temp-273.15)}°C`;
+    day5Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.list[39].weather[0].icon + '.png';
+};
