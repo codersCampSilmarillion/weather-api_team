910c569a8e3d2fd84f4b8bc1d2402ac70dab2059
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-11-27 21:21:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,4 @@ import {dailyPrognoseByLocation} from "./js/fiveDaysWeather";
 
 search();
 
-// //funkcja, która na podstawie wprowadzonego miasta przez użytkownika pokazuje odpowiednią pogode (należy dodać) i zmienia kolor tła(to już jest)
-// function searchWeatherByCity(searchedCity) {
-//     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&APPID=0599898336f8892a2625cd0151ec957c&units=metric`).then(result => {
-//         return result.json();
-//     }).then(result => {
-//         backgroundChange(result);
-//     })
-// }
-
-
 
