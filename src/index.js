006f842958ab6908b1012a70a2965131323618c8
misza <zielinskanoemi5@gmail.com>
2019-11-27 19:17:33 +0100

    Map added

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import {getDate, getTime} from "./js/getDate";
+import {getLocalization} from "./js/wetByLocation"
+import {getMap} from "./js/map";
+
+
+
+getDate();
+getTime();
+getLocalization();
+getMap();
