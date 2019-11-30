398588e85150fcf0ed402f60775603def1db40bb
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-11-30 20:24:50 +0100

    Update fiveDaysWeather.js

--- src/js/fiveDaysWeather.js
+++ src/js/fiveDaysWeather.js
@@ -1,3 +1,4 @@
+import { backgroundChange } from "./backgroundChange";
 import {getUv} from "./uvIndex";
 
 let day1 = document.getElementById("day1");
@@ -26,30 +27,30 @@ let day5Icon = document.getElementById("day5icon");
 
 //funkcja pobierająca dane o prognozie na 5dni na podstawie danych geograficznych
 function dailyPrognoseByLocation(api) {
-    const weather =[];
+    const weather = [];
     fetch(api)
         .then(result => {
-        return result.json();
-    }).then(data => {
-        weather.city = data.city.name;
-        weather.country = data.country;
-        weather.day1Temp = celsius(data.list[7].main.temp);
-        weather.day1icon = data.list[7].weather[0].icon;
-        weather.day2temp = celsius(data.list[15].main.temp);
-        weather.day2icon = data.list[15].weather[0].icon;
-        weather.day3temp = celsius(data.list[23].main.temp);
-        weather.day3icon = data.list[23].weather[0].icon;
-        weather.day4temp = celsius(data.list[31].main.temp);
-        weather.day4icon = data.list[31].weather[0].icon;
-        weather.day5temp = celsius(data.list[39].main.temp);
-        weather.day5icon = data.list[39].weather[0].icon;
-        weather.lat = data.city.coord.lat
-        weather.lon = data.city.coord.lon
+            return result.json();
+        }).then(data => {
+            weather.city = data.city.name;
+            weather.country = data.country;
+            weather.day1Temp = celsius(data.list[7].main.temp);
+            weather.day1icon = data.list[7].weather[0].icon;
+            weather.day2temp = celsius(data.list[15].main.temp);
+            weather.day2icon = data.list[15].weather[0].icon;
+            weather.day3temp = celsius(data.list[23].main.temp);
+            weather.day3icon = data.list[23].weather[0].icon;
+            weather.day4temp = celsius(data.list[31].main.temp);
+            weather.day4icon = data.list[31].weather[0].icon;
+            weather.day5temp = celsius(data.list[39].main.temp);
+            weather.day5icon = data.list[39].weather[0].icon;
+            weather.lat = data.city.coord.lat;
+            weather.lon = data.city.coord.lon;
 
-    }).then(function () {
-        thisWeek(weather);
+        }).then(function() {
+            thisWeek(weather);
+        })
     })
-
 }
 
 //funckja zamieniająca kelwiny na stopnie celsjusza w przyblizeniu
@@ -67,32 +68,34 @@ function thisWeek(resultFromServer) {
     now.setDate(daysSume + 1);
     daysSume = now.getDate();
     day1Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`;
-    day1Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.day1icon + '.png';
+    day1Icon.src = '../../assets/WetIcons/' + resultFromServer.day1icon + '.svg';
 
     day2.innerHTML = `${days[now.getDay()+1]}`;
     day2Temp.innerHTML = resultFromServer.day2temp;
     now.setDate(daysSume + 1);
     daysSume = now.getDate();
     day2Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
-    day2Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.day2icon + '.png';
+    day2Icon.src = '../../assets/WetIcons/' + resultFromServer.day2icon + '.svg';
 
     day3.innerHTML = `${days[now.getDay()+1]}`;
     day3Temp.innerHTML = resultFromServer.day3temp;
     now.setDate(daysSume + 1);
     daysSume = now.getDate();
     day3Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
-    day3Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.day3icon + '.png';
+    day3Icon.src = '../../assets/WetIcons/' + resultFromServer.day3icon + '.svg';
 
     day4.innerHTML = `${days[now.getDay()+1]}`;
     day4Temp.innerHTML = resultFromServer.day4temp;
     now.setDate(daysSume + 1);
     daysSume = now.getDate();
     day4Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
-    day4Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.day4icon + '.png';
+    day4Icon.src = '../../assets/WetIcons/' + resultFromServer.day4icon + '.svg';
 
     day5.innerHTML = `${days[now.getDay()+1]}`;
     day5Temp.innerHTML = resultFromServer.day5temp;
     now.setDate(daysSume + 1);
+    day5Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`
+    day5Icon.src = '../../assets/WetIcons/' + resultFromServer.day5icon + '.svg';
     day5Date.innerHTML = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`;
     day5Icon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.day5icon + '.png';
 
@@ -101,4 +104,4 @@ function thisWeek(resultFromServer) {
 
 };
 
-export {dailyPrognoseByLocation, thisWeek};
+export { dailyPrognoseByLocation, thisWeek };
