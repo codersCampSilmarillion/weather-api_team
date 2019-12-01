b12a6519ac54926047472d650e604ec92f4a7290
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-12-01 14:30:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {getDate, getTime} from "./js/getDate";
 import {search} from "./js/search";
 import {getLocalization} from "./js/getLocalization";
-
+import "./scss/main.scss";
 
 
 getLocalization();
