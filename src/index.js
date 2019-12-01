cf32698fad67e203c97eabd79694a71647efe521
Marek <michalskimarek89@gmail.com>
2019-12-01 14:01:20 +0100

    add scss import to index.js and comment out imports in map.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import {getDate, getTime} from "./js/getDate";
-import {search} from "./js/search";
-import {getLocalization} from "./js/getLocalization";
-
-
+import { getDate, getTime } from "./js/getDate";
+import { search } from "./js/search";
+import { getLocalization } from "./js/getLocalization";
+import "./scss/main.scss";
 
 getLocalization();
 getDate();
